a55991bf64e73867a54c31228156e4ad9e622531
mellowCaribou <33252035+mellowCaribou@users.noreply.github.com>
2019-08-10 16:36:07 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,10 +1,6 @@
 from all import Model, dataloaders, run_test
 import torch
 
-model = Model('/storage/resnet_pretrained.pth', True)
-fitted_model = model.fit(dataloaders, 42) 
-run_test(fitted_model, dataloaders)
-torch.save(fitted_model, "/storage/resnet_xray_transfered.model")
 
 for i in range(4):
     model = Model()
