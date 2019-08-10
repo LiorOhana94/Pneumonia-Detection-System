9410e9a338d43125809058be803a1eacb9363fcc
mellowCaribou <33252035+mellowCaribou@users.noreply.github.com>
2019-08-10 20:24:15 +0300

    Update test_existing_model.py

--- test_existing_model.py
+++ test_existing_model.py
@@ -1,5 +1,5 @@
 from all import Model, dataloaders, run_test
 import torch
 
-model = torch.load("/storage/resnet_xray_transfered_new.model")
+model = torch.load("/storage/vgg16_xray_transfered_new.model")
 run_test(model, dataloaders)
