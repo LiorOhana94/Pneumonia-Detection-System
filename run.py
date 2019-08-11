13e498fb0e8e035121904d795c45bf83be19d08d
mellowCaribou <33252035+mellowCaribou@users.noreply.github.com>
2019-08-11 09:48:31 +0300

    train vgg

--- run.py
+++ run.py
@@ -6,5 +6,5 @@ for i in range(4):
     model = Model()
     lr = i + 2
     fitted_model = model.fit(dataloaders, 42, lr) 
-    run_test(fitted_model, dataloaders)
+    run_test(fitted_model, dataloaders, "vgg16_xray_fitted_lr%d.model" % (lr))
     torch.save(fitted_model, "/storage/vgg16_xray_fitted_lr%d.model" % (lr))
