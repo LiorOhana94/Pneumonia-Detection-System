dc37f856ace093e14466860f447455f4af9cb829
mellowCaribou <33252035+mellowCaribou@users.noreply.github.com>
2019-08-30 22:32:48 +0300

    predict can now return a map image name

--- predict.py
+++ predict.py
@@ -6,7 +6,12 @@ import torchvision.transforms as transforms
 import torch
 import numpy as np
 
-model = torch.load(os.path.join("current_model/resnet_xray_fitted.model"), map_location='cpu')
+from skimage.transform import resize
+from skimage.io import imshow, imsave
+
+import matplotlib.pyplot as plt
+
+from cam.network.utils import SaveFeatures, imshow_transform
 
 
 test_loader = transforms.Compose([
@@ -16,7 +21,7 @@ test_loader = transforms.Compose([
     transforms.Normalize([0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
 
-def predict(model, image_path):
+def predict(model, image_path, generate_map=False):
     train_on_gpu = False # torch.cuda.is_available()
     model.eval()   
     model.cpu()
@@ -26,8 +31,39 @@ def predict(model, image_path):
             image = image.cuda()
             model.cuda()
         
+        if generate_map:
+            sf = SaveFeatures(model.features[-3])
+
         outputs = model(image)
-        return outputs
+
+        if generate_map:
+            heatmap_file_name = create_heatmap_file(sf, outputs, image, image_path)
+        
+        res = torch.argmax(outputs.data).cpu().detach().numpy()
+
+
+        return [res, heatmap_file_name]
+
+
+def create_heatmap_file(sf, outputs, image, image_path):
+    sf.remove()
+    arr = sf.features.cpu().detach().numpy()
+    features_data = arr[0]
+    res = torch.argmax(outputs.data).cpu().detach().numpy()
+    print(res, outputs.data)
+    ans = np.dot(np.rollaxis(features_data,0,3), [ res, res])
+    ans = resize(ans, (224,224))
+    plt.figure()
+    plt.subplots(figsize=(4,4))
+    plt.imshow(imshow_transform(image))
+    plt.imshow(ans, alpha=.4, cmap='jet')
+    cam_path = f'./class-activation-maps/{get_file_name(image_path)}.cam.png'
+    plt.savefig(cam_path)
+    return cam_path
+
+
+def get_file_name(file_path):
+    return os.path.splitext(os.path.basename(file_path))[0]
 
 
 test_loader = transforms.Compose([
@@ -50,9 +86,13 @@ def image_loader(loader, image_name):
 
 def to_rgb(im):
     # I think this will be slow
-    w, h = im.shape
-    ret = np.empty((w, h, 3), dtype=np.uint8)
-    ret[:, :, 0] = im
-    ret[:, :, 1] = im
-    ret[:, :, 2] = im
+    ret = im
+
+    if(len(im.shape) < 3):
+        w, h = im.shape
+        ret = np.empty((w, h, 3), dtype=np.uint8)
+        ret[:, :, 0] = im
+        ret[:, :, 1] = im
+        ret[:, :, 2] = im
+
     return ret
---
+++ produce_map.py
@@ -0,0 +1,46 @@
+import torch
+import numpy as np
+import os
+from skimage.transform import resize
+from skimage.io import imshow, imsave
+
+import matplotlib.pyplot as plt
+
+
+from predict import predict, image_loader, test_loader
+from cam.network.utils import SaveFeatures, imshow_transform
+
+model = torch.load('./storage/models/best_acc_vgg19_v2_100e.model')
+image_file = './temp-images/0faedaf7-d644-416c-a704-74fc0763ba76.jpeg'
+
+for image in os.listdir('./temp-images/'): 
+    image_file = f'./temp-images/{image}'
+    print(predict(model,image_file,generate_map=True))
+
+
+
+"""
+res = torch.argmax(outputs.data).cpu().detach().numpy()
+labels = ["healthy", "pneumonia"]
+
+print(f'result: {res} - {labels[res]} ')
+
+sf.remove()
+arr = sf.features.cpu().detach().numpy()
+features_data = arr[0]
+
+ans_0 = np.dot(np.rollaxis(features_data,0,3), [1,0])
+ans_1 = np.dot(np.rollaxis(features_data,0,3), [0,1])
+
+if(res ==1):
+    ans_1 = resize(ans_1, (224,224))
+    ans1_int = ans_1.astype(np.uint8)
+    plt.imsave('cam.png', ans_1, cmap='jet')
+
+im = image_loader(test_loader, image_file)
+
+plt.figure()
+plt.subplots(figsize=(4,4))
+plt.imshow(imshow_transform(im))
+plt.imshow(ans_1, alpha=.4, cmap='jet')
+my_image = plt.savefig('camcam.png')"""
