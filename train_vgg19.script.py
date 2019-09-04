7fe23d2c9a5f6060727abe2c5461136f7a473c0e
mellowCaribou <33252035+mellowCaribou@users.noreply.github.com>
2019-09-05 00:36:41 +0300

    Update train_vgg19.script.py

--- train_vgg19.script.py
+++ train_vgg19.script.py
@@ -24,7 +24,7 @@ from cam.network.utils import Flatten, accuracy, imshow_transform, SaveFeatures
 
 num_epochs = 150
 lr =.0001
-wd =.06
+wd =.1
 model_name = f"resnet19_{num_epochs}e_{lr}lr"
 # ---------------------------------- #
 
