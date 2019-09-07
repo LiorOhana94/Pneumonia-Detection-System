e6f33c21d3a73f7b182fdeb2263fc349f014859a
DESKTOP-QFH4CVL\lior <lior@storrsoft.com>
2019-09-07 17:38:55 +0300

    changed testing

--- test.py
+++ test.py
@@ -9,7 +9,10 @@ def run_test(model, dataloaders, model_name, testfile_prefix = ''):
     running_loss = 0.0
     running_corrects = 0
     items_num = 0
-
+    TP = 0
+    FP = 0
+    TN = 0
+    FN = 0
 
     for inputs, labels in dataloaders['test']['loader']:
         if train_on_gpu:
@@ -24,11 +27,22 @@ def run_test(model, dataloaders, model_name, testfile_prefix = ''):
 
             loss = criterion(outputs, labels)
         
+        TP += torch.sum(preds == labels.data and preds == 1)
+        FP += torch.sum(preds != labels.data and preds == 1)
+        TN += torch.sum(preds == labels.data and preds == 0)
+        FN += torch.sum(preds != labels.data and preds == 0)
+
+        accuracy = float(running_corrects)/items_num
+        recall = float(TP)/(TP + FN)
+        precision = float(TP)/(TP + FP)
         items_num += inputs.size(0)
         running_loss += loss.item() * inputs.size(0)
         running_corrects += torch.sum(preds == labels.data)
     f = open(f"/storage/tests_results/{testfile_prefix}_test_res_{model_name}.txt" ,"w+")
-    f.write(f"Test Results: we got {running_corrects} right out of {items_num}, ({float(running_corrects)/items_num:.2f}%)".format(running_corrects, items_num, float(running_corrects)/items_num))
+    f.write(f"Test Results: we got {running_corrects} right out of {items_num}, ({accuracy :.2f}%)\n")
+    f.write(f"Accuracy : {accuracy :.2f}%")
+    f.write(f"recall : {recall :.2f}%")
+    f.write(f"precision : {precision :.2f}%")
     f.close()
     return
 
@@ -38,3 +52,6 @@ def compare_after_loading(model, dataloaders, model_name):
     loaded_model = torch.load(f'/storage/models/{model_name}.model')
     run_test(loaded_model, dataloaders, model_name, 'after_load')
     return
+
+
+run_test
--- test_existing_model.py
+++ test_existing_model.py
@@ -3,44 +3,7 @@ import torch
 import torch.nn as nn
 import torchvision
 from cam.network.net import VGG, make_layers
+from test import run_test
+from model import model
 
-
-num_epochs = 200
-lr =.001
-model_name = f"resnet19_{num_epochs}e_{lr}lr" 
-
-def run_test(model, dataloaders, model_name):
-    train_on_gpu = torch.cuda.is_available()
-    criterion = nn.NLLLoss()
-    model.eval()   
-    running_loss = 0.0
-    running_corrects = 0
-    items_num = 0
-
-
-    for inputs, labels in dataloaders['test']['loader']:
-        if train_on_gpu:
-            inputs = inputs.cuda()
-            labels = labels.cuda()
-            model.cuda()
-
-       
-        with torch.set_grad_enabled(False):
-            outputs = model(inputs)
-            _, preds = torch.max(outputs, 1)
-
-            loss = criterion(outputs, labels)
-        
-        items_num += inputs.size(0)
-        running_loss += loss.item() * inputs.size(0)
-        running_corrects += torch.sum(preds == labels.data)
-    f = open("/storage/tests_results/test_res_%s.txt" % model_name,"w+")
-    f.write("Test Results: we got {0} right out of {1}, ({2:.2f}%)".format(running_corrects, items_num, float(running_corrects)/items_num))
-    f.close()
-    return
-
-# almog is in the house
-
-
-model = torch.load(f'/storage/models/{model_name}.model')
-run_test(model, dataloaders, f'{model_name}_after_load')
+run_test(model, dataloaders, 'my_new_test_on_model')
