271f9c6eab638c851c702af92f9a68f42964b3a7
DESKTOP-QFH4CVL\lior <lior@storrsoft.com>
2019-09-07 18:51:13 +0300

    Update test.py

--- test.py
+++ test.py
@@ -13,6 +13,7 @@ def run_test(model, dataloaders, model_name, testfile_prefix = ''):
     FP = 0
     TN = 0
     FN = 0
+    f = open(f"/storage/tests_results/{testfile_prefix}_test_res_{model_name}.txt" ,"w+")
 
     for inputs, labels in dataloaders['test']['loader']:
         if train_on_gpu:
@@ -32,17 +33,18 @@ def run_test(model, dataloaders, model_name, testfile_prefix = ''):
         TN += torch.sum(preds - labels.data*2 == 0)
         FN += torch.sum(preds - labels.data*2 == -2)
 
+        f.write(f"{TP}, {FP}, {TN}, {FN}\n")
+
         recall = float(TP)/(TP + FN)
         precision = float(TP)/(TP + FP)
         items_num += inputs.size(0)
         accuracy = float(running_corrects)/items_num
         running_loss += loss.item() * inputs.size(0)
         running_corrects += torch.sum(preds == labels.data)
-    f = open(f"/storage/tests_results/{testfile_prefix}_test_res_{model_name}.txt" ,"w+")
-    f.write(f"Test Results: we got {running_corrects} right out of {items_num}, ({accuracy :.2f}%)\n")
-    f.write(f"Accuracy : {accuracy :.2f}%")
-    f.write(f"recall : {recall :.2f}%")
-    f.write(f"precision : {precision :.2f}%")
+    f.write(f"Test Results: we got {running_corrects} right out of {items_num}\n")
+    f.write(f"Accuracy : {accuracy :.2f}\n")
+    f.write(f"Recall : {recall :.2f}\n")
+    f.write(f"Precision : {precision :.2f}\n")
     f.close()
     return
 
