5d3d9206c2471c83e6b9c97a4e90c9f2439e2100
mellowCaribou <33252035+mellowCaribou@users.noreply.github.com>
2019-09-13 13:51:42 +0300

    Update train_resnet50.script.py

--- train_resnet50.script.py
+++ train_resnet50.script.py
@@ -25,7 +25,7 @@ from cam.network.utils import Flatten, accuracy, imshow_transform, SaveFeatures
 # ----- Training Configuration ----- #
 date = datetime.datetime.now()
 time_str = date.strftime("%m%d%H%M")
-num_epochs = 8
+num_epochs = 14
 lr =.0001
 wd =.0001
 loss='nll'
